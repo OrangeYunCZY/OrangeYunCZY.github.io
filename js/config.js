1ea31cc34260ea152b1b90275ebe8749a28182cb
OrangeYun <40051058+OrangeYunCZY@users.noreply.github.com>
2021-09-22 22:21:45 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,18 @@
+var ip = "http://localhost:8080";
+var config = {
+    login: ip + "/login",
+    warehouse: ip + "/warehouse",
+    warehouse_wid: ip + "/warehouse_wid",
+    output: ip + "/output",
+    warehouse_post: ip + "/warehouse_post",
+    input: ip + "/input",
+    user: ip + "/user",
+    adduser: ip + "/adduser",
+    deluser: ip + "/deluser",
+    addclass: ip + "/addclass",
+    delclass: ip + "/delclass",
+    editclass: ip + "/editclass",
+    ew: ip + "/ew",
+    out: ip + "/out",
+    in: ip + "/in",
+};
